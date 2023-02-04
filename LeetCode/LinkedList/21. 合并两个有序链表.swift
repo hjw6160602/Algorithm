be438013fee83c4a256885f92e7e43337e1d0f91
hejiawei <hejiawei@qiyi.com>
2023-02-04 12:41:12 +0800

    .prac: 21. 合并两个有序链表

--- LeetCode/LinkedList/21. 合并两个有序链表.swift
+++ LeetCode/LinkedList/21. 合并两个有序链表.swift
@@ -9,6 +9,25 @@
 import Foundation
 
 extension Solution {
+    
+    func mergeTwoListsP1(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        let dummy = ListNode(0)
+        var l1 = list1, l2 = list2, node = dummy
+        while l1 !== nil && l2 !== nil {
+            if l1!.val < l2!.val {
+                node.next = l1!
+                l1 = l1!.next
+            } else {
+                node.next = l2!
+                l2 = l2!.next
+            }
+            node = node.next!
+        }
+        node.next = (l1 === nil) ? l2 : l1
+        
+        return dummy.next
+    }
+    
     func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         let dummy = ListNode()
         var node = dummy
