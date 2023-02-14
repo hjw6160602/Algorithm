4c5740436e43d2fe0c4db54ee291f76a6798e33f
hejiawei <hejiawei@qiyi.com>
2023-02-14 21:37:59 +0800

    .prac: 最大子数组和

--- VSCode/53.最大子数组和.swift
+++ VSCode/53.最大子数组和.swift
@@ -6,9 +6,23 @@
 
 // @lc code=start
 class Solution {
-    func maxSubArray(_ nums: [Int]) -> Int {
 
+    func maxSubArray(_ nums: [Int]) -> Int {
+        guard nums.count > 1 else { return nums[0] }
+
+        var dp = Array(repeating:0, count: nums.count)
+
+        dp[0] = nums[0]
+        
+        var res = dp[0]
+        for i in 1..<nums.count {
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
+            res = max(res, dp[i])
+        }
+
+        return res
     }
+
 }
 // @lc code=end
 
