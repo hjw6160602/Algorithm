62f7cf846fcc2fa84746c9d4bf2871506ca3bb24
hejiawei <hejiawei@qiyi.com>
2023-03-04 23:31:42 +0800

    .add: 除法求值

---
+++ LeetCode/图/UnionFind.swift
@@ -0,0 +1,55 @@
+//
+//  UnionFind.swift
+//  Array
+//
+//  Created by 贺嘉炜 on 2023/3/4.
+//  Copyright © 2023 saidicaprio. All rights reserved.
+//
+
+import Foundation
+
+public class UF {
+    // 连通分量个数
+    private(set) var count = 0
+    
+    // 存储每个节点的父节点
+    private var parent = [Int]()
+    
+    // n 为图中节点的个数
+    init(_ n: Int = 0) {
+        count = n
+        parent = Array(repeating: 0, count: n)
+        // 每个节点都在初始化的时候指向自己
+        for i in 0..<n {
+            parent[i] = i
+        }
+    }
+    
+    public func union(p: Int, q: Int) {
+        let rootP = find(p)
+        let rootQ = find(q)
+        if rootP == rootQ {
+            return
+        }
+        // 将q所在的分量的根节 点指向p所在分量的根节点
+        parent[rootQ] = rootP
+        // 两个连通分量合并成一个连通分量
+        count -= 1
+    }
+    
+    // 判断节点 p 和节点 q 是否连通
+    public func connected(p: Int, q: Int) -> Bool {
+        let rootP = find(q)
+        let rootQ = find(q)
+        return rootP == rootQ
+    }
+    
+    // 核心方法 边查找边进行路径压缩
+    public func find(_ x: Int) -> Int {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+    
+}
---
+++ LeetCode/图/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  图
+//
+//  Created by 贺嘉炜 on 2023/3/4.
+//  Copyright © 2023 saidicaprio. All rights reserved.
+//
+
+import Foundation
+
+let LeetCode = Solution.shared
+
+class Solution {
+    static let shared = Solution()
+    
+    private init() {}
+}
+
+test399calcEquation()
+
